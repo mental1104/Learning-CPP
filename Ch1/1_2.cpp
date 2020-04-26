53e0fceb5d89d91c841cd033eb3542924c4a8399
mental1104 <mental1104@gmail.com>
2020-04-26 16:07:34 +0800

    1.1 - 1.2 complete

---
+++ Ch1/1_2.cpp
@@ -0,0 +1,11 @@
+int main()
+{
+    return -1;
+}
+
+/**********************************************************
+ * ubuntu@VM-0-5-ubuntu:~/Project/Cpp-Primer/Ch1$ ./a.out
+ * ubuntu@VM-0-5-ubuntu:~/Project/Cpp-Primer/Ch1$ echo $?
+ * 255
+ *
+ **********************************************************/
