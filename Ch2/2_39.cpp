2723447ffd9d65ec956430a880bf7260abfc830a
mental1104 <mental1104@gmail.com>
2020-04-30 15:50:45 +0800

    Chapter2 complete

---
+++ Ch2/2_39.cpp
@@ -0,0 +1,11 @@
+struct Foo{}
+int main(){
+    return 0;
+}
+
+/******************************************
+ 2_39.cpp:1:13: error: expected ‘;’ after struct definition
+ struct Foo{}
+             ^
+             ;
+******************************************/
