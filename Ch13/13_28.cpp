271f0dd4322470809eb65a4342c1b72ce17a2d6f
mental1104 <mental1104@gmail.com>
2020-05-07 17:24:34 +0800

    oh man

---
+++ Ch13/13_28.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+
+class TreeNode {
+    TreeNode()
+        : value(string()), count(new int(1)), left(nullptr), right(nullptr)
+    {}
+
+    TreeNode(const TreeNode& rhs)
+        : value(rhs.value), count(rhs.count), left(rhs.left), right(rhs.right)
+    {
+        ++*count;
+    }
+
+    TreeNode& operator=(const TreeNode& rhs);
+    ~TreeNode()
+    {
+        if (--*count == 0) {
+            if (left) {
+                delete left;
+                left = nullptr;
+            }
+            if (right) {
+                delete right;
+                right = nullptr;
+            }
+            delete count;
+            count = nullptr;
+        }
+    }
+
+
+private:
+    std::string  value;
+    int         *count;
+    TreeNode    *left;
+    TreeNode    *right;
+
+};
