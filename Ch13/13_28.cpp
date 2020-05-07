509239cd5f1dd0fb1f6bb1a076aa71ea81532399
mental1104 <mental1104@gmail.com>
2020-05-07 18:01:57 +0800

    13.2

--- Ch13/13_28.cpp
+++ Ch13/13_28.cpp
@@ -1,39 +1,32 @@
-#include<iostream>
+#include "ex13_28.h"
 
-
-class TreeNode {
-    TreeNode()
-        : value(string()), count(new int(1)), left(nullptr), right(nullptr)
-    {}
-
-    TreeNode(const TreeNode& rhs)
-        : value(rhs.value), count(rhs.count), left(rhs.left), right(rhs.right)
-    {
-        ++*count;
-    }
-
-    TreeNode& operator=(const TreeNode& rhs);
-    ~TreeNode()
-    {
-        if (--*count == 0) {
-            if (left) {
-                delete left;
-                left = nullptr;
-            }
-            if (right) {
-                delete right;
-                right = nullptr;
-            }
-            delete count;
-            count = nullptr;
+TreeNode& TreeNode::operator=(const TreeNode& rhs)
+{
+    ++*rhs.count;
+    if (--*count == 0) {
+        if (left) {
+            delete left;
+            left = nullptr;
         }
+        if (right) {
+            delete right;
+            right = nullptr;
+        }
+
+        delete count;
+        count = nullptr;
     }
+    value = rhs.value;
+    left = rhs.left;
+    right = rhs.right;
+    count = rhs.count;
+    return *this;
+}
 
-
-private:
-    std::string  value;
-    int         *count;
-    TreeNode    *left;
-    TreeNode    *right;
-
-};
+BinStrTree& BinStrTree::operator=(const BinStrTree& bst)
+{
+    TreeNode* new_root = new TreeNode(*bst.root);
+    delete root;
+    root = new_root;
+    return *this;
+}
