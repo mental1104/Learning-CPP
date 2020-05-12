23dc28cfaeff44be6a95025df8f17162c6a2d2c9
mental1104 <mental1104@gmail.com>
2020-05-12 09:41:09 +0800

    15.2.1

---
+++ Ch15/15_3(Quote).h
@@ -0,0 +1,23 @@
+class Quote {
+public:
+    Quote() = default;
+    Quote(cosnt std::string &book, double sales_price):
+                    bookNo(book), price(sales_price) {}  
+    std::string isbn() const { return bookNo; }
+
+    virtual double net_price(std::size_t n) const       
+                    { return n * price; }
+    virtual ~Quote() = default;
+private:
+    std::string bookNo;
+protected:
+    double price = 0.0;
+}
+
+double print_total(ostream &os, const Quote &item, size_t n)
+{
+    double ret = item.net_price(n);
+    os << "ISBN: " << item.isbn() 
+       << " # sold: " << n << " total due: " << ret << endl;
+    return ret;
+}
