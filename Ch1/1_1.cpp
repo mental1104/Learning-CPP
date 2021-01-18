5b294232d1e3603069ac9365165169a4c15fb42b
ubuntu <mental1104@gmail.com>
2021-01-18 14:08:17 +0800

    Return to cpp-primer

--- Ch1/1_1.cpp
+++ Ch1/1_1.cpp
@@ -1,7 +1,7 @@
 /*
- * ubuntu@VM-0-5-ubuntu:~/Project/Cpp-Primer$ g++ --version
- * g++ (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0
- * Copyright (C) 2017 Free Software Foundation, Inc.
+ * ubuntu@VM-0-5-ubuntu:~/espeon/cpp$ g++ --version
+ * g++ (Ubuntu 9.3.0-11ubuntu0~18.04.1) 9.3.0
+ * Copyright (C) 2019 Free Software Foundation, Inc.
  * This is free software; see the source for copying conditions.  There is NO
  * warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
